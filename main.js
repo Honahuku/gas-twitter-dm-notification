3b995c18e306105202a7f95ab115e5f04b63e13b
Yosuke Yamakawa <tech@y-yamakawa.com>
2022-07-15 10:20:22 +0900

    メール整形処理の変更

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-function fetchContactMail() {
-    /* Gmailから特定条件のスレッドを検索しメールを取り出す */
-    var strTerms = 'label:twitter';
-    var myThreads = GmailApp.search(strTerms, 0, 10); //条件にマッチしたスレッドを取得
-    var myMsgs = GmailApp.getMessagesForThreads(myThreads); //スレッドからメールを取得する　→二次元配列で格納  var valMsgs = [];
+// 参考サイト
+// https://qiita.com/guchimina/items/9bb040b507d8a6cc59a3
 
-    var br = /[\r\n]+/g; //改行
-    var rep = ''; //置換文字列
-    /* 各メールから日時、送信元、件名、内容を取り出す*/
-    for (var j = 0; j < myMsgs.length; j++) {
-        for (var i = 0; i < myMsgs[j].length; i++) {
-            if (!myMsgs[j][i].isStarred()) {
-                valMsgs[i] = myMsgs[j][i].getBody().replace(br, ''); //html形式の取得と空白削除
-                var myRegexp =
-                    /<td class=\"preheader\" style=\"padding:0;margin:0;line-height:1px;font-size:1px;font-size:1px;color:#ffffff;\">([\s\S]*?)<d><\/d><\/td>/;
-                //↑tdタグで囲われた場所を抽出するための正規表現。gmailだとstyleで書かれるため大変汚い。
-                var text = valMsgs[i].match(myRegexp); // matchを用いると0に素のテキスト1に抽出したテキストが入る
-                var date = myMsgs[j][i].getDate();
-                var formatD = Utilities.formatDate(date, 'Asia/Tokyo', 'yyyy-MM-dd HH:mm'); //見やすいようにフォーマットをかける
-                myMsgs[j][i].star(); // starをつけることで次回以降引っかからないように
+var FindSubject = 'label:twitter';
 
-                console.log(text[1]);
-                console.log(date);
-                console.log(formatD);
+function getMail() {
+    //指定した件名のスレッドを検索して取得
+    var myThreads = GmailApp.search(FindSubject);
+    //スレッドからメールを取得し二次元配列に格納
+    var myMessages = GmailApp.getMessagesForThreads(myThreads);
+
+    for (var i in myMessages) {
+        for (var j in myMessages[i]) {
+            //スターがないメッセージのみ処理
+            if (!myMessages[i][j].isStarred()) {
+                var strDate = myMessages[i][j].getDate();
+                var strSubject = myMessages[i][j].getSubject();
+                var oriMessage = myMessages[i][j].getBody();
+                var cutMessage = oriMessage.match(
+                    /1px;font-size:1px;color:#ffffff;"> [\s\S]*?<d>/g
+                );
+                var strMessage = cutMessage[0]
+                    .replace('1px;font-size:1px;color:#ffffff;">', '')
+                    .replace('<d>', '');
+                Logger.log(strMessage);
+                Logger.log(strDate);
+                Logger.log(strSubject);
+
+                //処理済みのメッセージをスターをつける
+                myMessages[i][j].star();
             }
         }
     }
